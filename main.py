27c93d231479b5e5b2a24669bcd9d73fa43ca08a
Christian <marshmallow.19008@gmail.com>
2024-01-19 05:37:48 +0200

    Sessionmaker + def __str__

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import sqlalchemy
 
+from sqlalchemy.orm import sessionmaker
+
+from models import create_tables, Genre
+
 DSN = 'postgresql://postgres:valentina1923@localhost:5432/MusicAppDB'
 engine = sqlalchemy.create_engine(DSN)
+
+create_tables(engine)
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+genre1 = Genre(genre_name='Hip-hop')
+print(genre1.id)
+
+session.add(genre1)
+session.commit()
+
+print(genre1.id)
+print(genre1)
+session.close()
--- models.py
+++ models.py
@@ -4,21 +4,26 @@ from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
-DSN = 'postgresql://postgres:valentina1923@localhost:5432/MusicAppDB'
-engine = sq.create_engine(DSN)
-
 class Genre(Base):
     __tablename__ = 'genre'
     id = sq.Column(sq.Integer, primary_key=True)
-    genre_name = sq.Column(sq.String(length=64), nullable=False)
+    genre_name = sq.Column(sq.String(length=64), nullable=False, unique=True)
+
+    def __str__(self):
+        return f'Genre ID: {self.id} Name: {self.genre_name}'
 
 class Artist(Base):
     __tablename__ = 'artist'
+
     id = sq.Column(sq.Integer, primary_key=True)
     artist_name = sq.Column(sq.String(length=64), nullable=False)
 
+    def __str__(self):
+        return f'Artist ID: {self.id} Name: {self.artist_name}'
+
 class Genre_Artist(Base):
     __tablename__ = 'genre_artist'
+
     id = sq.Column(sq.Integer, primary_key=True)
     genre_id = sq.Column(sq.Integer, sq.ForeignKey('genre.id'), nullable=False)
     artist_id = sq.Column(sq.Integer, sq.ForeignKey('artist.id'), nullable=False)
@@ -28,12 +33,17 @@ class Genre_Artist(Base):
 
 class Album(Base):
     __tablename__ = 'album'
+
     id = sq.Column(sq.Integer, primary_key=True)
     album_title = sq.Column(sq.String(length=64), nullable=False)
     album_release_year = sq.Column(sq.Integer, nullable=False)
 
+    def __str__(self):
+        return f'Album ID: {self.id} Title: {self.album_title} Release_Year: {self.album_release_year}'
+
 class Artist_Album(Base):
     __tablename__ = 'artist_album'
+
     id = sq.Column(sq.Integer, primary_key=True)
     artist_id = sq.Column(sq.Integer, sq.ForeignKey('artist.id'), nullable=False)
     album_id = sq.Column(sq.Integer, sq.ForeignKey('album.id'), nullable=False)
@@ -43,6 +53,7 @@ class Artist_Album(Base):
 
 class Song(Base):
     __tablename__ = 'song'
+
     id = sq.Column(sq.Integer, primary_key=True)
     song_name = sq.Column(sq.String(length=64), nullable=False)
     song_length = sq.Column(sq.Interval, nullable=False)
@@ -50,14 +61,22 @@ class Song(Base):
 
     album = relationship(Album, backref='song')
 
+    def __str__(self):
+        return f'Song ID: {self.id} Name: {self.song_name} Length: {self.song_length}'
+
 class Compilation(Base):
     __tablename__ = 'compilation'
+
     id = sq.Column(sq.Integer, primary_key=True)
     comp_title = sq.Column(sq.String(length=64), nullable=False)
     comp_release_year = sq.Column(sq.Integer, nullable=False)
 
+    def __str__(self):
+        return f'Compilation ID: {self.id} Title: {self.comp_title} Release_Year: {self.comp_release_year}'
+
 class Song_Compilation(Base):
     __tablename__ = 'song_compilation'
+
     id = sq.Column(sq.Integer, primary_key=True)
     song_id = sq.Column(sq.Integer, sq.ForeignKey('song.id'), nullable=False)
     compilation_id = sq.Column(sq.Integer, sq.ForeignKey('compilation.id'), nullable=False)
@@ -66,8 +85,8 @@ class Song_Compilation(Base):
     compilation = relationship(Compilation, backref='song_compilation')
 
 def create_tables(engine):
-    # Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
-create_tables(engine)
+
 
