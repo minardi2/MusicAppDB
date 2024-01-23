a9b43dbbc4f773afb65fae56fa0864344bc393f7
Christian <marshmallow.19008@gmail.com>
2024-01-23 21:57:15 +0200

    Service

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sqlalchemy
 
 from sqlalchemy.orm import sessionmaker
 
-from models import create_tables, Genre
+from models import create_tables, Genre, Artist
 
 DSN = 'postgresql://postgres:valentina1923@localhost:5432/MusicAppDB'
 engine = sqlalchemy.create_engine(DSN)
@@ -10,8 +10,27 @@ engine = sqlalchemy.create_engine(DSN)
 create_tables(engine)
 
 Session = sessionmaker(bind=engine)
-
 session = Session()
+
+class Service:
+
+    def __init__(self, session):
+        self.session = session
+
+    def create_genre(self, name):
+        genre = Genre(genre_name=name)
+        session.add(genre)
+        session.commit()
+        return genre
+
+    def create_artist(self, name):
+        artist = Artist(artist_name=name)
+        session.add(artist)
+        session.commit()
+        return artist
+
+
+
 
 genre1 = Genre(genre_name='Hip-hop')
 print(genre1.id)
